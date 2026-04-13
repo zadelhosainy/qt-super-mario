df5f9d07f8e66b03d77b1368d54d557bd6a7ff28
Zad <zad.elhosainy@aucegypt.edu>
2026-04-13 17:33:16 +0000

    added two platforms

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,16 @@ int main(int argc, char* argv[]) {
   ground.setBrush(Qt::darkGreen);
   ground.setPos(100, 250);
   scene.addItem(&ground);
+  
+  QGraphicsRectItem platform1(0, 0, 150, 20);
+  platform1.setBrush(Qt::darkGreen);
+  platform1.setPos(400, 200);
+  scene.addItem(&platform1);
+ 
+  QGraphicsRectItem platform2(0, 0, 150, 20);
+  platform2.setBrush(Qt::darkGreen);
+  platform2.setPos(700, 150);
+  scene.addItem(&platform2);
 
   QGraphicsView view(&scene);
   view.setWindowTitle("Qt Super Mario");
